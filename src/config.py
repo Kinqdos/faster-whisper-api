a75981661e9ef68626429f612574158e017310b0
Carl Julian Sauter <sauter@siemens.com>
2025-06-28 21:02:40 +0300

    Improved config

--- src/config.py
+++ src/config.py
@@ -1,4 +1,6 @@
 import json
+import os.path
+import shutil
 
 from pydantic import BaseModel
 from pydantic import PositiveInt
@@ -7,10 +9,18 @@ from pydantic import PositiveInt
 class Config(BaseModel):
     model: str
     compute_type: str
-    device: str = "auto"
+    device: str
     max_processes: PositiveInt
 
-def load_config() -> Config:
-    with open('./config.json', 'r') as file:
+
+def load_config(path_or_dir: str) -> Config:
+    config_path = path_or_dir if path_or_dir.endswith(".json") else os.path.join(path_or_dir, "config.json")
+
+    # Copy default config, if no config is present
+    if not os.path.exists(config_path):
+        print("No config file found, creating default config")
+        shutil.copy2("./config.json", config_path)
+
+    with open(config_path, 'r') as file:
         data = json.load(file)
         return Config(**data)
